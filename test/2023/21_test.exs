a04f62e8d7b9adc85bfff91a1bf346d6cf340df4
Isaiah Rondeau <mail@irondeau.com>
2023-12-21 02:39:06 -0600

    Y2023 D21 (revised)

--- test/2023/21_test.exs
+++ test/2023/21_test.exs
@@ -21,13 +21,8 @@ defmodule AdventOfCodeTest.Y2023.D21Test do
     assert {42, _} = Puzzle.run(@garden_plot)
   end
 
+  @tag :slow
   test "solution" do
-    # something like 4.8478 * 10^14
-    # for steps
-    #   f(65)           -> 3759
-    #   f(65 + 131)     -> 33496
-    #   f(65 + 131 * 2) -> 92857
-    # gives 606188414811259
     assert {3646, 606188414811259} = Puzzle.run()
   end
 end
