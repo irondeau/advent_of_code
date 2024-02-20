e64b3a73b98fdbf7262c929b7e728de4d6e8e112
Isaiah Rondeau <mail@irondeau.com>
2024-02-20 16:27:14 -0600

    pq: add comparator function

--- lib/helpers/pq.ex
+++ lib/helpers/pq.ex
@@ -1,26 +1,36 @@
 defmodule AdventOfCode.Helpers.PQ do
   alias __MODULE__
 
-  defstruct heap: nil
-
-  @typedoc "A pairing heap node containing an item and a priority."
-  @type heap_node :: {term(), number()}
+  defstruct heap: nil, comparator: &Kernel.</2
 
   @typedoc "A pairing heap."
   @type heap :: %{
-          node: heap_node(),
+          node: term(),
           subheaps: [heap()]
         }
 
   @typedoc "A priority queue."
   @type t :: %__MODULE__{
-          heap: heap()
+          heap: heap(),
+          comparator: (term(), term() -> boolean())
         }
+
+  @spec empty?(t()) :: boolean()
+  def empty?(%PQ{} = pq), do: pq.heap == nil
 
   @spec merge(t(), t()) :: t()
   def merge(%PQ{heap: nil}, pq), do: pq
   def merge(pq, %PQ{heap: nil}), do: pq
-  def merge(%PQ{} = pq_1, %PQ{} = pq_2), do: %PQ{heap: meld(pq_1.heap, pq_2.heap)}
+
+  def merge(%PQ{comparator: cmp_1}, %PQ{comparator: cmp_2}) when cmp_1 != cmp_2,
+    do: raise(ArgumentError, message: "comparator functions do not match")
+
+  def merge(%PQ{} = pq_1, %PQ{} = pq_2) do
+    %PQ{
+      heap: meld(pq_1.heap, pq_2.heap, pq_1.comparator),
+      comparator: pq_1.comparator
+    }
+  end
 
   @doc """
   Returns a new empty priority queue.
@@ -33,44 +43,47 @@ defmodule AdventOfCode.Helpers.PQ do
   """
   @spec new() :: t()
   def new(), do: %PQ{}
+  def new(comparator) when is_function(comparator, 2), do: %PQ{comparator: comparator}
 
   @spec peek(t()) :: term()
   def peek(pq, default \\ nil)
   def peek(%PQ{heap: nil}, default), do: default
-  def peek(%PQ{heap: %{node: {item, _}}}, _default), do: item
+  def peek(%PQ{heap: %{node: item}}, _default), do: item
 
   @spec pop(t()) :: {term(), t()}
   def pop(pq, default \\ nil)
   def pop(%PQ{heap: nil} = pq, default), do: {default, pq}
-  def pop(%PQ{heap: %{node: {item, _}, subheaps: []}}, _default), do: {item, %PQ{}}
 
-  def pop(%PQ{heap: %{node: {item, _}, subheaps: [subheap]}}, _default),
-    do: {item, %PQ{heap: subheap}}
+  def pop(%PQ{heap: %{node: item, subheaps: []}, comparator: comparator}, _default),
+    do: {item, %PQ{comparator: comparator}}
 
-  def pop(%PQ{heap: %{node: {item, _}, subheaps: subheaps}}, _default) do
+  def pop(%PQ{heap: %{node: item, subheaps: [subheap]}, comparator: comparator}, _default),
+    do: {item, %PQ{heap: subheap, comparator: comparator}}
+
+  def pop(%PQ{heap: %{node: item, subheaps: subheaps}, comparator: comparator}, _default) do
     heap =
       subheaps
       |> Enum.chunk_every(2)
       |> Enum.map(fn pair ->
         with [heap_1, heap_2] <- pair do
-          meld(heap_1, heap_2)
+          meld(heap_1, heap_2, comparator)
         else
           [heap] -> heap
         end
       end)
       |> Enum.reverse()
-      |> Enum.reduce(&meld/2)
+      |> Enum.reduce(&(meld(&1, &2, comparator)))
 
-    {item, %PQ{heap: heap}}
+    {item, %PQ{heap: heap, comparator: comparator}}
   end
 
-  @spec push(t(), term(), number()) :: t()
-  def push(%PQ{} = pq, item, priority), do: merge(pq, leaf(item, priority))
+  @spec push(t(), term()) :: t()
+  def push(%PQ{} = pq, item), do: merge(pq, leaf(item, pq.comparator))
 
-  defp leaf(item, priority), do: %PQ{heap: %{node: {item, priority}, subheaps: []}}
+  defp leaf(item, comparator), do: %PQ{heap: %{node: item, subheaps: []}, comparator: comparator}
 
-  defp meld(heap_1, heap_2) do
-    if elem(heap_1.node, 1) <= elem(heap_2.node, 1) do
+  defp meld(heap_1, heap_2, comparator) do
+    if comparator.(heap_1.node, heap_2.node) do
       %{node: heap_1.node, subheaps: [heap_2 | heap_1.subheaps]}
     else
       %{node: heap_2.node, subheaps: [heap_1 | heap_2.subheaps]}
--- test/helpers/pq_test.exs
+++ test/helpers/pq_test.exs
@@ -3,32 +3,106 @@ defmodule AdventOfCodeTest.Helpers.PQTest do
 
   alias AdventOfCode.Helpers.PQ
 
+  test "merge" do
+    pq_1 =
+      PQ.new()
+      |> PQ.push(4)
+      |> PQ.push(3)
+      |> PQ.push(6)
+
+    assert PQ.merge(pq_1, PQ.new()) == pq_1
+    assert PQ.merge(PQ.new(), pq_1) == pq_1
+    assert PQ.merge(PQ.new() |> PQ.push(2), pq_1) != pq_1
+
+    pq_2 =
+      PQ.new()
+      |> PQ.push(1)
+      |> PQ.push(2)
+      |> PQ.push(5)
+      |> PQ.push(7)
+
+    pq = PQ.merge(pq_1, pq_2)
+
+    1..7
+    |> Enum.reduce(pq, fn item, pq ->
+      {next, pq} = PQ.pop(pq)
+      assert next == item
+      pq
+    end)
+    |> then(fn pq -> assert PQ.empty?(pq) end)
+  end
+
   test "insert" do
     pq =
       PQ.new()
-      |> PQ.push("hello", 2)
-      |> PQ.push("world", 3)
-      |> PQ.push("first", 1)
+      |> PQ.push(2)
+      |> PQ.push(3)
+      |> PQ.push(1)
+      |> PQ.push(4)
 
-    assert PQ.peek(pq) == "first"
+    assert PQ.peek(pq) == 1
   end
 
   test "pop" do
     pq =
       PQ.new()
-      |> PQ.push("a", 3)
-      |> PQ.push("b", 2)
-      |> PQ.push("c", 1)
-      |> PQ.push("d", 4)
-      |> PQ.push("e", 7)
-      |> PQ.push("f", 5)
-      |> PQ.push("g", 6)
+      |> PQ.push(3)
+      |> PQ.push(2)
+      |> PQ.push(1)
+      |> PQ.push(4)
+      |> PQ.push(7)
+      |> PQ.push(5)
+      |> PQ.push(6)
 
-    ~w(c b a d f g e)
+    1..7
     |> Enum.reduce(pq, fn item, pq ->
-      {min, pq} = PQ.pop(pq)
-      assert min == item
+      {next, pq} = PQ.pop(pq)
+      assert next == item
       pq
     end)
+    |> then(fn pq -> assert PQ.empty?(pq) end)
+  end
+
+  test "comparator" do
+    comparator =
+      fn str_1, str_2 ->
+        String.to_integer(str_1, 16) > String.to_integer(str_2, 16)
+      end
+
+    pq =
+      PQ.new(comparator)
+      |> PQ.push("990")
+      |> PQ.push("51d")
+      |> PQ.push("3f3")
+      |> PQ.push("ba7")
+      |> PQ.push("558")
+
+    ~w(ba7 990 558 51d 3f3)
+    |> Enum.reduce(pq, fn item, pq ->
+      {next, pq} = PQ.pop(pq)
+      assert next == item
+      pq
+    end)
+    |> then(fn pq -> assert PQ.empty?(pq) end)
+  end
+
+  test "bad comparator" do
+    assert_raise FunctionClauseError, fn ->
+      PQ.new(fn _ -> false end)
+    end
+  end
+
+  test "non-matching comparator functions" do
+    comparator =
+      fn str_1, str_2 ->
+        String.to_integer(str_1, 16) > String.to_integer(str_2, 16)
+      end
+
+    pq_1 = PQ.new() |> PQ.push(4)
+    pq_2 = PQ.new(comparator) |> PQ.push("990")
+
+    assert_raise ArgumentError, fn ->
+      PQ.merge(pq_1, pq_2)
+    end
   end
 end
