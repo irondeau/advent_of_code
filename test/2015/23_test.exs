030839e4f57bf02b2cbf566ab6561c477b8770ee
Isaiah Rondeau <mail@irondeau.com>
2024-11-26 15:49:42 -0600

    Y2015 D23: fix test case

--- test/2015/23_test.exs
+++ test/2015/23_test.exs
@@ -4,6 +4,6 @@ defmodule AdventOfCodeTest.Y2015.D23Test do
   alias AdventOfCode.Y2015.D23, as: Puzzle
 
   test "solution" do
-    assert {255, nil} = Puzzle.run()
+    assert {255, 334} = Puzzle.run()
   end
 end
