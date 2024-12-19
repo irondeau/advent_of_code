18bac7d35f689fe9e738adda449392d7a0b9354b
Isaiah Rondeau <mail@irondeau.com>
2024-12-18 23:49:14 -0600

    Y2024 D19

---
+++ test/2024/19_test.exs
@@ -0,0 +1,27 @@
+defmodule AdventOfCodeTest.Y2024.D19Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode.Y2024.D19, as: Puzzle
+
+  @towel_designs """
+  r, wr, b, g, bwu, rb, gb, br
+
+  brwrr
+  bggr
+  gbbr
+  rrbgbr
+  ubwu
+  bwurrg
+  brgr
+  bbrgwb
+  """
+
+  test "examples" do
+    assert {6, 16} = Puzzle.run(@towel_designs)
+  end
+
+  @tag :slow
+  test "solution" do
+    assert {300, 624802218898092} = Puzzle.run()
+  end
+end
