a01b2d90641dad3a0444760dd1a659a9ed908a2b
Isaiah Rondeau <mail@irondeau.com>
2025-12-07 12:09:55 -0600

    mix task cleanup

--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -11,22 +11,29 @@ defmodule Mix.Tasks.Solve do
   @impl Mix.Task
   def run(args) do
     case Mix.AdventOfCode.parse(args) do
-      {[help: true], _, _} -> Mix.shell.info("Usage: #{@usage}")
-      {[year: year, day: day], _, _} -> 
+      {[help: true], _, _} ->
+        Mix.shell().info("Usage: #{@usage}")
+
+      {[year: year, day: day], _, _} ->
         case AdventOfCode.solve(year, day) do
           {:ok, {result_pt_1, result_pt_2}} ->
-            Mix.shell().info("Result:\n\s\sPart 1: #{result_pt_1}\n\s\sPart 2: #{result_pt_2}")
+            Mix.shell().info("""
+            Result:
+              Part 1: #{result_pt_1}
+              Part 2: #{result_pt_2}
+            """)
+
           {:ok, result} ->
             Mix.shell().info("Result: #{result}")
-          # {:error, :solution_out_of_bounds} ->
-          #   Mix.shell().error("Invalid date #{day}/#{year}. Date must match "
-          #     <> "#{AdventOfCode.first_year()} <= year <= #{AdventOfCode.last_year()} and "
-          #     <> "#{AdventOfCode.first_day()} <= day <= #{AdventOfCode.last_day}")
+
           {:error, :solution_not_found} ->
             Mix.shell().info("Solution for puzzle #{day}/#{year} has not yet been implemented")
         end
+
       {opts, _, _} ->
-        Mix.shell().error("Invald arguments. Got #{opts |> OptionParser.to_argv() |> Enum.join(" ")}; expected both --year and --day")
+        Mix.shell().error(
+          "Invald arguments. Got #{opts |> OptionParser.to_argv() |> Enum.join(" ")}; expected both --year and --day"
+        )
     end
   end
 end
