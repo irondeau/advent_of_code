7790a253785bb5a2bf8ee7dd1d581ba17539da3b
Isaiah Rondeau <mail@irondeau.com>
2025-12-09 01:20:35 -0600

    Y2025 D9 P1

---
+++ test/2025/09_test.exs
@@ -0,0 +1,24 @@
+defmodule AdventOfCodeTest.Y2025.D9Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode.Y2025.D9, as: Puzzle
+
+  @tiles """
+  7,1
+  11,1
+  11,7
+  9,7
+  9,5
+  2,5
+  2,3
+  7,3
+  """
+
+  test "examples" do
+    assert {50, 24} = Puzzle.run(@tiles)
+  end
+
+  test "solution" do
+    assert {4_748_985_168, nil} = Puzzle.run()
+  end
+end
